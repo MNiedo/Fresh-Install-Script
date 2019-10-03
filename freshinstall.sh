2894cb07c7a352f1b239dc3dc79df64b6883e4d2
MNiedo <mmniedostatkiewicz+github@gmail.com>
2019-10-04 00:31:14 +0200

    Add files via upload
    First release.
    Added formatting to easily see changes.

---
+++ freshinstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt update
+echo "==========================="
+echo "Repos Updated."
+echo "==========================="
+apt install traceroute
+echo "==========================="
+echo "Installed 1: Traceroute"
+echo "==========================="
+apt install nmap
+echo "==========================="
+echo "Installed 2: mnmap"
+echo "==========================="
+apt install gparted
+echo "==========================="
+echo "Installed 3: Gparted"
+echo "==========================="
+apt install vlc
+echo "==========================="
+echo "Installed 4: vlc"
+echo "==========================="
+apt install wireshark
+echo "==========================="
+echo "Installed 5: Wireshark"
+echo "==========================="
