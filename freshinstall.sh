70867e8aa4e2b5334cb1b96b3ef741e830e6552c
MNiedo <mmniedostatkiewicz+github@gmail.com>
2020-03-12 00:05:50 +0100

    Update freshinstall.sh
    Added -y parameter to turn off the "are you sure you want to install this package?" questions.

--- freshinstall.sh
+++ freshinstall.sh
@@ -3,23 +3,23 @@ apt update
 echo "==========================="
 echo "Repos Updated."
 echo "==========================="
-apt install traceroute
+apt install traceroute -y
 echo "==========================="
 echo "Installed 1: Traceroute"
 echo "==========================="
-apt install nmap
+apt install nmap -y
 echo "==========================="
 echo "Installed 2: mnmap"
 echo "==========================="
-apt install gparted
+apt install gparted -y
 echo "==========================="
 echo "Installed 3: Gparted"
 echo "==========================="
-apt install vlc
+apt install vlc -y
 echo "==========================="
 echo "Installed 4: vlc"
 echo "==========================="
-apt install wireshark
+apt install wireshark -y
 echo "==========================="
 echo "Installed 5: Wireshark"
 echo "==========================="
