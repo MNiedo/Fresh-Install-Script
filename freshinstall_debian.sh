3330ac94ea0984cb21b520ec0d3d2390362ff1d4
MNiedo <mmniedostatkiewicz+github@gmail.com>
2020-03-12 00:22:25 +0100

    Add files via upload
    Debian bare system edition.

---
+++ freshinstall_debian.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+apt update
+echo "==========================="
+echo "Repos Updated."
+echo "==========================="
+apt install net-tools -y
+echo "==========================="
+echo "Installed 0.01: net-tools"
+apt install file -y
+echo "==========================="
+echo "Installed 0.02: file"
+apt install bash-completion -y
+echo "==========================="
+echo "Installed 0.03: bash-completion"
+apt install firmware-atheros -y
+echo "==========================="
+echo "Installed 0.04: firmware-atheros"
+apt install man-db -y
+echo "==========================="
+echo "Installed 0.05: man-db"
+apt install manpages -y
+echo "==========================="
+echo "Installed 0.06: manpages"
+apt install openssh-client -y
+echo "==========================="
+echo "Installed 0.07: openssh-client"
+apt install python3 -y
+echo "==========================="
+echo "Installed 0.08: python3"
+apt install telnet -y
+echo "==========================="
+echo "Installed 0.09: telnet"
+apt install wget -y
+echo "==========================="
+echo "Installed 0.10: wget"
+apt install traceroute -y
+echo "==========================="
+echo "Installed 1: Traceroute"
+echo "==========================="
+apt install nmap -y
+echo "==========================="
+echo "Installed 2: mnmap"
+echo "==========================="
+apt install gparted -y
+echo "==========================="
+echo "Installed 3: Gparted"
+echo "==========================="
+apt install vlc -y
+echo "==========================="
+echo "Installed 4: vlc"
+echo "==========================="
+apt install wireshark -y
+echo "==========================="
+echo "Installed 5: Wireshark"
+echo "==========================="
